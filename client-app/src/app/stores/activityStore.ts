65c6f7467ec55795ce574b2872c61754af490e11
ken <ken.fontaine@outlook.fr>
2020-03-17 22:20:37 +0100

    end of vid 111

--- client-app/src/app/stores/activityStore.ts
+++ client-app/src/app/stores/activityStore.ts
@@ -17,10 +17,21 @@ class ActivityStore {
 
     // ******     Definition des computed *****************
     @computed get activitiesByDate() {
-        return Array.from(this.activityRegistry.values()).sort((a, b) => Date.parse(a.date) - Date.parse(b.date)); 
-        // activityRegistry n'es pas un array donc je peux pas sort. Du coup je le convertie avant
+        return this.groupActivitiesByDate(Array.from(this.activityRegistry.values())); 
+        // activityRegistry n'es pas un array donc je peux pas sort. Du coup je le convertie avant !!
     }
 
+    // ****** Helper Fonction ***********
+    groupActivitiesByDate(activities: IActivity[]){
+        const sortedActivities = activities.sort(
+            (a, b) => Date.parse(a.date) - Date.parse(b.date)
+        )
+        return Object.entries(sortedActivities.reduce((activities, activity) => {
+            const date = activity.date.split('T')[0];
+            activities[date] = activities[date] ? [...activities[date], activity] : [activity];
+            return activities;
+        }, {} as { [key:string] : IActivity[] }));
+    }
 
     // *******     Definition des actions *******
 
